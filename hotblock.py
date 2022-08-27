31375467a10cdfe5205f38cbc75eb457266a5870
HELLO-PC\Lihq <coolfreemail@sohu.com>
2022-08-28 00:34:48 +0800

    修改

--- hotblock.py
+++ hotblock.py
@@ -32,40 +32,32 @@ while (rs.error_code == '0') & rs.next():
 
 isTradeday = data_list[0][1]
 
-if isTradeday == "1":
-    with open("config/config.json",encoding="utf-8") as f:
-        cfg = json.load(f)
-    info = cfg["mysql"]
-
-    cnx = pymysql.connect(user=info["user"],password=info["password"],host=info["host"],database=info["database"])
-    cur_block = cnx.cursor()
-    cur_block_select = "select block,prevdate from tdx.hotblock where block=%s;"
-
+if isTradeday == "0":
     url = "https://www.eastmoney.com"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, "
                              "like Gecko) Chrome/104.0.0.0 Safari/537.36"}
-
     response = requests.get(url=url,headers=headers)
     soup = BeautifulSoup(response.content,"lxml")
-
     contents = soup.select(".ts-hot-con a")
+    block_list = [];
     for item in contents:
         block = re.findall('(.+(?=板块))',item.text)
         if block != []:
-            cur_block.execute(cur_block_select,block)
-            database_block = cur_block.fetchone()
-            if database_block is not None:
-                if database_block[1] != today:
-                    # 更新prevdate和count
-                    days = (today - datetime.datetime.strptime(database_block[1],"%Y-%m-%d").date()).days
-                    cur_block_update = "update `hotblock` set `prevdate`='%s',`count`=%d where `block`='%s';"%(today.strftime("%Y-%m-%d"),days,block[0])
-                    cur_block.execute(cur_block_update)
-                    cnx.commit()
-            else:
-                # 插入当前板块信息 (block,prevdate,count)
-                cur_block_insert = "insert into `hotblock`(`block`,`prevdate`,`count`) values('%s','%s',%d)" % (block[0], today, 0)
-                cur_block.execute(cur_block_insert)
-                cnx.commit()
+            block_list.append(block[0])
+
+    with open("config/config.json",encoding="utf-8") as f:
+        cfg = json.load(f)
+    info = cfg["mysql"]
+    cnx = pymysql.connect(user=info["user"],password=info["password"],host=info["host"],database=info["database"])
+
+    cur_block = cnx.cursor()
+    cur_block_select = "select block,date from tdx.block_v;"
+ #   update hotblock set date = adddate(date, interval 1 day) where(block, date) in (select block, date from block_v);
+    cur_block.execute(cur_block_select)
+    data_block = cur_block.fetchall()
+    for item in data_block:
+        if(item[0]  in block_list):
+            print(item[0])
     cur_block.close()
     cnx.close()
 
