a3074d2d83b52f18cdd671da38f797848db84fa8
HELLO-PC\Lihq <coolfreemail@sohu.com>
2022-08-30 12:55:35 +0800

    修改2

--- hotblock.py
+++ hotblock.py
@@ -32,18 +32,20 @@ while (rs.error_code == '0') & rs.next():
 
 isTradeday = data_list[0][1]
 
-if isTradeday == "0":
+if isTradeday == "1":
     url = "https://www.eastmoney.com"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, "
                              "like Gecko) Chrome/104.0.0.0 Safari/537.36"}
     response = requests.get(url=url,headers=headers)
     soup = BeautifulSoup(response.content,"lxml")
     contents = soup.select(".ts-hot-con a")
-    block_list = [];
+    block_web_list = [];
     for item in contents:
         block = re.findall('(.+(?=°å¿é))',item.text)
         if block != []:
-            block_list.append(block[0])
+            block_web_list.append(block[0])
+
+    print(block_web_list)
 
     with open("config/config.json",encoding="utf-8") as f:
         cfg = json.load(f)
@@ -51,14 +53,14 @@ if isTradeday == "0":
     cnx = pymysql.connect(user=info["user"],password=info["password"],host=info["host"],database=info["database"])
 
     cur_block = cnx.cursor()
-    cur_block_select = "select block,date from tdx.block_v;"
- #   update hotblock set date = adddate(date, interval 1 day) where(block, date) in (select block, date from block_v);
-    cur_block.execute(cur_block_select)
-    data_block = cur_block.fetchall()
-    for item in data_block:
-        if(item[0]  in block_list):
-            print(item[0])
+    cur_block_update = "update hotblock set days = days+1 where(block, date) in (select block, date from block_v);";
+    cur_block.execute(cur_block_update)
+
+    cur_block_insert = "insert into hotblock(block,date,days) values('%s','%s','%d');"
+    for item in block_web_list:
+        cur_block.execute(cur_block_insert%(item,today,0))
     cur_block.close()
+    cnx.commit()
     cnx.close()
 
 bs.logout()
