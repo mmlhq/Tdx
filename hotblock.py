59f30a6edb3d7dbf98df1d9a61a5ad371604e22d
HELLO-PC\Lihq <coolfreemail@sohu.com>
2022-09-01 22:21:23 +0800

    热门板块

--- hotblock.py
+++ hotblock.py
@@ -8,21 +8,19 @@ import re
 import baostock as bs
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-
-
 def import_data():
     today = datetime.date.today()
 
     #### 登陆系统 ####
     lg = bs.login()
     # 显示登陆返回信息
-    print('login respond error_code:'+lg.error_code)
-    print('login respond  error_msg:'+lg.error_msg)
+    print('login respond error_code:' + lg.error_code)
+    print('login respond  error_msg:' + lg.error_msg)
 
     #### 获取交易日信息 ####
     rs = bs.query_trade_dates(start_date=today)
-    print('query_trade_dates respond error_code:'+rs.error_code)
-    print('query_trade_dates respond  error_msg:'+rs.error_msg)
+    print('query_trade_dates respond error_code:' + rs.error_code)
+    print('query_trade_dates respond  error_msg:' + rs.error_msg)
 
     data_list = []
     while (rs.error_code == '0') & rs.next():
@@ -35,21 +33,25 @@ def import_data():
         url = "https://www.eastmoney.com"
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, "
                                  "like Gecko) Chrome/104.0.0.0 Safari/537.36"}
-        response = requests.get(url=url,headers=headers)
-        soup = BeautifulSoup(response.content,"lxml")
+        response = requests.get(url=url, headers=headers)
+        soup = BeautifulSoup(response.content, "lxml")
         contents = soup.select(".ts-hot-con a")
         block_web_list = [];
         for item in contents:
-            block = re.findall('(.+(?=板块))',item.text)
+            block = re.findall('(.+(?=板块))', item.text)
+            industry = re.findall('(.+(?=行业))', item.text)
             if block != []:
                 block_web_list.append(block[0])
+            if industry != []:
+                block_web_list.append(industry[0])
 
         print(block_web_list)
 
-        with open("config/config.json",encoding="utf-8") as f:
+        with open("config/config.json", encoding="utf-8") as f:
             cfg = json.load(f)
         info = cfg["mysql"]
-        cnx = pymysql.connect(user=info["user"],password=info["password"],host=info["host"],database=info["database"])
+        cnx = pymysql.connect(user=info["user"], password=info["password"], host=info["host"],
+                              database=info["database"])
 
         cur_block = cnx.cursor()
         cur_block_update = "update hotblock set days = days+1 where(block, date) in (select block, date from block_v);";
@@ -57,7 +59,7 @@ def import_data():
 
         cur_block_insert = "insert into hotblock(block,date,days) values('%s','%s','%d');"
         for item in block_web_list:
-            cur_block.execute(cur_block_insert%(item,today,0))
+            cur_block.execute(cur_block_insert % (item, today, 0))
         cur_block.close()
         cnx.commit()
         cnx.close()
